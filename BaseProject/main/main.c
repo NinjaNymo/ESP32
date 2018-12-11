284cf16db1214c15cded5d921a3c01933b43d608
Nikolai Nymo <nikolainymo@gmail.com>
2018-12-11 22:59:56 +0100

    Complete "Hello World!", flashes and works

---
+++ BaseProject/main/main.c
@@ -0,0 +1,14 @@
+//   _____ _____ _____
+//  |   | |__   |   | | ESP32/BaseProject/main/main.c
+//  | | | |   __| | | | Nikolai Nymo
+//  |_|___|_____|_|___| 11-12-18
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+
+void app_main(){
+    printf("Hello World!\n");
+}
